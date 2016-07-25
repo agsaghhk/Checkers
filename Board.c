0c554b7dd5596450704d1fda4800f32f79cecd98
agsagshhk <agsaghhk@yahoo.com>
2016-07-25 20:04:40 +0000

    removed failure, correct name

--- Board.c
+++ Board.c
@@ -3,8 +3,7 @@ struct Board {
    int board[8][8];
 };
 
-void HelloWorld( int board[][8]){
-   (void)fprintf(stdout,"Hello World\n");
+void initialize( int board[][8]){
    for(int i=0;i<8;i++){
       for(int I=0;I<8;I++){
          board[i][I]=0;
@@ -13,12 +12,4 @@ void HelloWorld( int board[][8]){
       (void)fprintf(stdout,"\n");
    }
 }
-/*void initialize( struct Board checkerBoard){
-      for(int index=0;index<boardSize;index++){
-         for(int Index=0;Index<boardSize;Index++){
-            checkerBoard.board[index][Index]=0;
-         }
-      }
-   }*/
-
 
--- Checkers.c
+++ Checkers.c
@@ -3,13 +3,6 @@
 int main(int argc, char argv[]){
    int board[8][8];
    struct Board checkerBoard;
-   HelloWorld(checkerBoard.board);
-   //HelloWorld(board);
-   /*struct Board board;
-   initialize(board.board);
-   for(int space=0;space<8; space++){
-      for(int Space=0; Space<8; Space++){
-         (void)fprintf(stdout,"%d",board.board[space][Space]);
-      }
-   }*/
+   initialize(checkerBoard.board);
+   
 }
